e1708fafa8c35fcdc066a8c1079f8b5fa53a83e9
Rithvika Kathroju <91027820+rithvika-k@users.noreply.github.com>
2024-01-01 02:23:44 -0500

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -1,35 +1,5 @@
-/*
-Rithvika Kathroju 
-11/1/2022
-ICS 4U1-01
-
-Unit 2 Assignment
-
-Option 1: The Nickname Generator Program
--- Kahoot Triva Simulator 
-The player is given a list of 25 uniquely generated nicknames. Here, you will 
-also be able to add, search, and or delete from the list of nicknames. Each player
-will then pick a player nickname and then proceed to answer 5 triva questions. 
-After each question, points will be awarded and a scoreboard will be 
-displayed. At the end of the game a podium with the top three players will be 
-displayed. The objective is to get the most amount of questions right to WIN!
-
-NOTES README!
-- implemented 2D arrays for the questionnaire aspect
-- restructured code to implement classes, complete with attributes, constructors, and methods
-  --- Player class which acts as a storage to hold key attributes of any Player object created
-      helps keep code organized and make working with the attributes of any Player object easier
-- an extended java class
-  --- KahootPlayer subclass which extends from the Player class. It includes methods more specific
-      to Kahoot players as opposed to any other player (i.e. number of questions correct, or if the
-      player got a question right)
-- created a method that checks to see if there are any duplicate nicknames generated
-  --- program also prevents duplicate trivia questions from being outputted
-      prevent user from adding a nickname that already exists
-      prevent user from deleting too many nicknames (4 nicknames should remain, one for each player)
-- used an ArrayList<> to add/delete (among other things) from an array
-  --- ArrayList<String> nicknames (array that holds the 25 generated nicknames)
-*/
+// Rithvika Kathroju 
+// 11/1/2022
 
 import java.util.Scanner; // import Scanner class
 import java.util.Arrays; // import Arrays class
